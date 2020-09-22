1e68de96de7f0aa1666e2176d785489401db5e04
Yuriuh <642192014@qq.com>
2020-09-22 23:27:54 +0800

    Isomorphic code

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Link from 'next/link'
 
+console.log('执行了')
 export default function post() {
+  // Node 环境不会执行
+  const clickMe = useCallback(() => {
+    console.log('You clicked me')
+  })
   return (
     <div>
       <div>First Post</div>
       <Link href="/">
         <a>回到首页</a>
       </Link>
+      <hr />
+      <button onClick={clickMe}>click me</button>
     </div>
   )
 }
