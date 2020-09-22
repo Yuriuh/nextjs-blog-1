54b187cc8558774a98ad93e30cc63ceacbdfe1f8
Yuriuh <642192014@qq.com>
2020-09-23 00:20:01 +0800

    Use scss

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,15 @@
 import React, { useCallback } from 'react'
 import Link from 'next/link'
+import styles from 'styles/first-post.module.scss'
 
 console.log('执行了')
 export default function post() {
-  // Node 环境不会执行
-  const clickMe = useCallback(() => {
-    console.log('You clicked me')
-  })
   return (
-    <div>
-      <div>First Post</div>
+    <div className={styles.wrapper}>
+      <div className={styles.content}>内容</div>
       <Link href="/">
         <a>回到首页</a>
       </Link>
-      <hr />
-      <button onClick={clickMe}>click me</button>
     </div>
   )
 }
