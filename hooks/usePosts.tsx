ddb066d7b7ef5c1e3b57948bd20394d3b0070662
Yuriuh <642192014@qq.com>
2020-09-23 03:08:48 +0800

    Add Client rendering demo, and use hooks

---
+++ hooks/usePosts.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+type Post = {
+  id: string
+  date: string
+  title: string
+}
+
+export const usePosts = () => {
+  const [posts, setPosts] = useState<Post[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isEmpty, setIsEmpty] = useState(false)
+  useEffect(() => {
+    setIsLoading(true)
+    axios.get('/api/v1/posts').then(
+      res => {
+        if (res.data.length === 0) {
+          setIsEmpty(true)
+        }
+        setTimeout(() => {
+          setIsLoading(false)
+          setPosts(res.data)
+        }, 2000)
+      },
+      () => {
+        setIsLoading(false)
+      }
+    )
+  }, [])
+  return {
+    posts,
+    setPosts,
+    isLoading,
+    setIsLoading,
+    isEmpty,
+    setIsEmpty,
+  }
+}
